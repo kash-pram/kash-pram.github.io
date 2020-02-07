92d0f229a17647c5bc8f2e5c59dd7aa1b04e8131
ppm1988 <ppm.career@gmail.com>
2020-02-07 06:16:33 +0530

    trial with cloud and github widget

--- src/app/modules/skillset/skillset.component.ts
+++ src/app/modules/skillset/skillset.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { CloudData, CloudOptions } from 'angular-tag-cloud-module';
 
 @Component({
   selector: 'app-skillset',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skillset.component.scss']
 })
 export class SkillsetComponent implements OnInit {
-
+  // options: CloudOptions = {
+  //   // if width is between 0 and 1 it will be set to the size of the upper element multiplied by the value 
+  //   width: 1000,
+  //   height: 400,
+  //   overflow: false,
+  // };
+ 
+  // data: CloudData[] = [
+  //   { text: 'weight-5-rotate(+10)', weight: 5, rotate: 10 },
+  //   { text: 'weight-7-rotate(-20)', weight: 7, rotate: -20 },
+  //   { text: 'weight-9-rotate(+35)', weight: 9, rotate: 35 }
+  //   // ...
+  // ];
+  
   constructor() { }
 
   ngOnInit() {
--- src/app/modules/skillset/skillset.module.ts
+++ src/app/modules/skillset/skillset.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SkillsetComponent } from './skillset.component';
 import { SkillsetRoutingModule } from './skillset-routing.module';
+// import { TagCloudModule } from 'angular-tag-cloud-module';
 
 @NgModule({
   declarations: [SkillsetComponent],
   imports: [
     CommonModule,
+    // TagCloudModule,
     SkillsetRoutingModule
   ]
 })
