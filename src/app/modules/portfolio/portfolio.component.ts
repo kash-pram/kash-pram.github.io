6d2dacabee1c9fe59f40f432d6e901c034f0dd32
ppm1988 <ppm.career@gmail.com>
2020-02-07 06:35:28 +0530

    trialed medium widget

--- src/app/modules/portfolio/portfolio.component.ts
+++ src/app/modules/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import {BrowserModule, DomSanitizer} from '@angular/platform-browser'
 
 @Component({
   selector: 'app-portfolio',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
+  // scriptHTML;
 
-  constructor() { }
+  constructor () {
+  // constructor ( private sanitizer: DomSanitizer) {
+    // this.scriptHTML = sanitizer.bypassSecurityTrustHtml('<div id="medium-widget"></div><script src="https://medium-widget.pixelpoint.io/widget.js"></script>\
+    // <script>MediumWidget.Init({renderTo: "#medium-widget", params: {"resource":"https://medium.com/@ppm1988/loading-webpages-faster-6950a65c6d90","postsPerLine":2,"limit":4,"picture":"big","fields":["description","author","claps","publishAt"],"ratio":"landscape"}})</script>') ;
+  }
 
   ngOnInit() {
   }
--- src/app/modules/portfolio/portfolio.module.ts
+++ src/app/modules/portfolio/portfolio.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PortfolioComponent } from './portfolio.component';
 import { PortfolioRoutingModule } from './portfolio-routing.module';
+// import { MediumWidgetModule } from 'ngx-medium-widget';
 
 @NgModule({
   declarations: [PortfolioComponent],
   imports: [
     CommonModule,
+    // MediumWidgetModule,
     PortfolioRoutingModule
   ]
 })
