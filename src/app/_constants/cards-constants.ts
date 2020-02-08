431f7b58718d558dc89c03ee8f8bf0fd9e9e30d7
ppm1988 <ppm.career@gmail.com>
2020-02-08 17:23:48 +0530

    style changes nav nav-tabs

--- src/app/_constants/cards-constants.ts
+++ src/app/_constants/cards-constants.ts
@@ -1,7 +1,6 @@
 export const CARDS_CONSTANTS = {
   '/profiles': [{
     title: 'StackOverflow',
-    subTitle: 'Developer forum',
     innerHTML: '<a class="no-outline" href="https://stackoverflow.com/users/2959821/ppm1988" target="_blank"><img src="https://stackoverflow.com/users/flair/2959821.png?theme=clean" width="208" height="58" alt="" title=""></a>'
   }],
   '/portfolio': [],
