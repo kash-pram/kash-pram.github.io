971a7d70793e6b3b91b50c9110a4e1c2bde6ce39
ppm1988 <ppm.career@gmail.com>
2020-02-08 19:37:41 +0530

    added social link images

--- src/app/_constants/cards-constants.ts
+++ src/app/_constants/cards-constants.ts
@@ -2,23 +2,33 @@ export const CARDS_CONSTANTS = {
   '/profiles': [
     {
       title: 'StackOverflow',
-      innerHTML: '<a class="no-outline" href="https://stackoverflow.com/users/2959821/ppm1988" target="_blank"><img src="https://stackoverflow.com/users/flair/2959821.png?theme=clean" width="208" height="58" alt="" title=""></a>'
+      link: 'https://stackoverflow.com/users/2959821/ppm1988',
+      innerHTML: '<img src="https://stackoverflow.com/users/flair/2959821.png?theme=clean" width="208" height="58" alt="" title="">'
     },
     {
       title: 'Github',
-      innerHTML: '<a class="no-outline" href="" target="_blank"></a>'
+      link: 'https://github.com/ppm1988/',
+      innerHTML: '<img src="/assets/logos/GitHub-Mark-64px.png" height="58" alt="" title="">'
+    },
+    {
+      title: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/ppm1988',
+      innerHTML: '<img src="assets/logos/LI-In-Bug.png" height="58" alt="" title="">'
     },
     {
       title: 'Medium',
-      innerHTML: '<a class="no-outline" href="" target="_blank"></a>'
+      link: 'https://medium.com/@ppm1988',
+      innerHTML: '<img src="assets/logos/Monogram.svg" height="58" alt="" title="">'
     },
-    {
-      title: 'Facebook',
-      innerHTML: '<a class="no-outline" href="" target="_blank"></a>'
-    },
+    // {
+    //   title: 'Facebook',
+    //   link: 'https://www.facebook.com/ppm1988',
+    //   innerHTML: '<img src="assets/logos/f_logo_RGB-Blue_58.png" height="58" alt="" title="">'
+    // },
     {
       title: 'Twitter',
-      innerHTML: '<a class="no-outline" href="" target="_blank"></a>'
+      link: 'https://twitter.com/social_ppm',
+      innerHTML: '<img src="assets/logos/Twitter_Social_Icon_Circle_Color.svg" height="58" alt="" title="">'
     }
   ],
   '/portfolio': [],
