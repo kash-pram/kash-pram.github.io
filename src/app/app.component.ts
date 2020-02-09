681db4cb72c0f4e6c1d678570b40ca5e39a736fb
ppm1988 <ppm.career@gmail.com>
2020-02-10 00:09:40 +0530

    added scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -107,5 +107,15 @@ constructor (
 
   } // CONSTRUCTOR
   
+  onActivate(event) {
+    let scrollToTop = window.setInterval(() => {
+      let pos = window.pageYOffset;
+      if (pos > 0) {
+          window.scrollTo(0, pos - 20); // how far to scroll on each step
+      } else {
+          window.clearInterval(scrollToTop);
+      }
+    }, 16);
+  } // FN
 
 } // EXPORTS
