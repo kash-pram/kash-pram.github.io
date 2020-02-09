2f55b59e210b9ee4eb07fed2d8552c385aa5d252
ppm1988 <ppm.career@gmail.com>
2020-02-09 15:53:16 +0530

    added location strategy

--- src/app/_constants/cards-constants.ts
+++ src/app/_constants/cards-constants.ts
@@ -10,19 +10,19 @@ export const CARDS_CONSTANTS = {
       title: 'Github',
       // subTitle: 'assets/icons/external-link.svg',
       link: 'https://github.com/ppm1988/',
-      innerHTML: '<img src="/assets/logos/GitHub-Mark-64px.png" height="58" alt="" title="">'
+      innerHTML: '<img src="/assets/logos/GitHub-Mark-64px.png" height="58" alt="Github" title="">'
     },
     {
       title: 'LinkedIn',
       // subTitle: 'assets/icons/external-link.svg',
       link: 'https://www.linkedin.com/in/ppm1988',
-      innerHTML: '<img src="assets/logos/LI-In-Bug.png" height="58" alt="" title="">'
+      innerHTML: '<img src="assets/logos/LI-In-Bug.png" height="58" alt="LinkedIn" title="">'
     },
     {
       title: 'Medium',
       // subTitle: 'assets/icons/external-link.svg',
       link: 'https://medium.com/@ppm1988',
-      innerHTML: '<img src="assets/logos/Monogram.svg" height="58" alt="" title="">'
+      innerHTML: '<img src="assets/logos/Monogram.svg" height="58" alt="Medium" title="">'
     },
     // {
     //   title: 'Facebook',
@@ -33,7 +33,7 @@ export const CARDS_CONSTANTS = {
       title: 'Twitter',
       // subTitle: 'assets/icons/external-link.svg',
       link: 'https://twitter.com/social_ppm',
-      innerHTML: '<img src="assets/logos/Twitter_Social_Icon_Circle_Color.svg" height="58" alt="" title="">'
+      innerHTML: '<img src="assets/logos/Twitter_Social_Icon_Circle_Color.svg" height="58" alt="Twitter" title="">'
     }
   ],
   '/projects': [],
