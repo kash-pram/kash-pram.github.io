5475294249e36bdec393967da40f863425e7f00d
ppm1988 <ppm.career@gmail.com>
2020-02-15 03:29:36 +0530

    added data from resume

--- src/app/_constants/past-constants.ts
+++ src/app/_constants/past-constants.ts
@@ -2,110 +2,154 @@ export const PROJECT_CARDS = [
   {
     title: 'Pulse Sales Tool',
     innerHTML: '',
+    type: 'MVC web application',
     status: 'Delivered',
-    for: 'CTS'
+    skills: ['ASP.NET', 'C#', 'MS SQL', 'HTML', 'CSS', 'Visual SourceSafe'],
+    for: 'Oshkosh [Logistics] - CTS'
   },
   {
     title: 'ETF Trading',
     innerHTML: '',
+    type: 'Front-end web applications',
     status: 'N/A',
-    for: 'CTS'
+    skills: ['HTML', 'CSS', 'JS', 'jQuery - AJAX', 'SVN'], 
+    for: 'BNY Mellon [Banking and Financial Services] - CTS'
   },
   {
     title: 'Sixth Day Allowance',
     innerHTML: '',
+    type: 'Front-end web applications',
     status: 'N/A',
-    for: 'CTS'
+    skills: ['HTML', 'CSS', 'JS', 'jQuery - AJAX', 'SVN'], 
+    for: 'BNY Mellon [Banking and Financial Services] - CTS'
   },
   {
     title: 'Kindle Device Metrics',
     innerHTML: '',
+    type: 'Full-stack web application',
     status: 'Delivered',
-    for: 'Amazon'
+    skills: ['HTML', 'JS', 'CSS', 'PHP', 'MySQL','jQuery - AJAX', 'Chart JS', 'JIRA', 'Perforce'], 
+    for: 'Amazon Internal'
   },
   {
     title: 'Record And Play',
     innerHTML: '',
+    type: 'Scripting',
     status: 'N/A',
-    for: 'Amazon'
+    skills: ['Python', 'Git', 'Jenkins'], 
+    for: 'Amazon Internal'
   },
   {
     title: '300 DPI Approach',
     innerHTML: '',
+    type: 'Scripting',
     status: 'N/A',
-    for: 'Amazon'
+    skills: ['Shell scripting'],
+    for: 'Amazon Internal'
+  },
+  {
+    title: 'FireOS My-Accounts',
+    innerHTML: '',
+    type: 'Testing',
+    status: 'N/A',
+    skills: ['Unit Testing', 'Android', 'JIRA', 'Git'], 
+    for: 'Quality Assurance - Amazon'
   },
   {
     title: 'RidePal Admin Dashboard',
     innerHTML: '',
+    type: 'Full-stack web application',
     status: 'Delivered',
-    for: 'CES IT'
+    skills: ['Angular JS 1.4', 'Google Maps', 'MySQL', 'Node JS', 'Scala', 'JIRA', 'Git'], 
+    for: 'RidePal [Corporate commute services] - CES IT'
   },
   {
     title: 'RidePal Official Website',
     innerHTML: '',
+    type: 'Front-end web applications',
     status: 'Delivered',
-    for: 'CES IT'
+    skills: ['Angular 8', 'Confluence', 'JIRA', 'Git'], 
+    for: 'RidePal [Corporate commute services] - CES IT'
   },
   {
     title: 'Vantage Dashboard Intellimetric',
     innerHTML: '',
+    type: 'Front-end web applications',
     status: 'N/A',
-    for: 'CES IT'
+    skills: ['Angular 8', 'Angular Material', 'Stripes', 'AWS Cognito', 'Amplify', 'Zeplin', 'BitBucket', 'Git'], 
+    for: 'Vantage [Learning services] - CES IT'
   },
   {
     title: 'Timer',
     innerHTML: '',
+    type: '',
     status: 'Work-in-progress',
-    for: 'self'
+    skills: [''], 
+    for: ''
   },
   {
     title: 'beppo',
     innerHTML: '',
+    type: '',
     status: 'N/A',
-    for: 'self'
+    skills: [''], 
+    for: ''
   },
   {
     title: 'tevoted',
     innerHTML: '',
+    type: '',
     status: 'Work-in-progress',
-    for: 'self'
+    skills: [''], 
+    for: ''
   },
   {
     title: 'ces-corner',
     innerHTML: '',
+    type: '',
     status: 'Work-in-progress',
-    for: 'self'
+    skills: [''], 
+    for: ''
   },
   {
     title: 'portfolio',
     innerHTML: '',
+    type: '',
     status: 'N/A',
-    for: 'self'
+    skills: [''], 
+    for: ''
   },
   {
     title: 'magic-box',
     innerHTML: '',
+    type: '',
     status: 'Work-in-progress',
-    for: 'self'
+    skills: [''], 
+    for: ''
   },
   {
     title: 'tototo',
     innerHTML: '',
+    type: '',
     status: 'Pending',
-    for: 'self'
+    skills: [''], 
+    for: ''
   },
   {
     title: 'need-basis / group notify',
     innerHTML: '',
+    type: '',
     status: 'Pending',
-    for: 'self'
+    skills: [''], 
+    for: ''
   },
   {
     title: 'work-log',
     innerHTML: '',
+    type: '',
     status: 'Pending',
-    for: 'self'
+    skills: [''], 
+    for: ''
   },
 ];
 
@@ -114,30 +158,40 @@ export const TIMELINE_CARDS = [
     title: 'Cognizant Technology Solutions India Pvt. Ltd.',
     duration: 'Feb 2011 - June 2013 [2 years 4 months]',
     logo: '',
-    innerHTML: 'Programmer Analyst Trainee, Programmer Analyst'
+    link: '',
+    projects: ['Pulse Sales Tool', 'ETF Trading', 'Sixth Day Allowance' ],
+    positions: ['Programmer Analyst Trainee', 'Programmer Analyst']
   },
   {
     title: 'Rush46',
     duration: 'June 2013 - Aug 2013 [3 months]',
     logo: '',
-    innerHTML: 'Co-Owner'
+    link: '',
+    projects: [],
+    positions: ['Co-Owner']
   },
   {
     title: 'Amazon Development Center India Pvt. Ltd.',
     duration: 'Sep 2013 - Feb 2015 [1 year 5 months]',
     logo: '',
-    innerHTML: 'Software Development Consultant, Quality Assurance Engineer'
+    link: '',
+    projects: ['Kindle Device Metrics', 'Record and Play', '300 DPI', 'FireOS My-Accounts'],
+    positions: ['Software Development Consultant', 'Quality Assurance Engineer']
   },
   {
     title: 'BEPPO INDIA',
     duration: 'Apr 2015 - Dec 2016 [1 year 8 months]',
     logo: '',
-    innerHTML: 'Owner'
+    link: '',
+    projects: [],
+    positions: ['Owner']
   },
   {
     title: 'CES Information Technologies Pvt. Ltd.',
     duration: 'March 2017 - Nov 2019 [2 years 8 months]',
     logo: '',
-    innerHTML: 'Software Engineer, Senior Software Technologist'
+    link: '',
+    projects: ['RidePal Admin Dashboard', 'RidePal Official Website', 'Vantage Admin Dashboard'],
+    positions: ['Software Engineer', 'Senior Software Technologist']
   }
 ];
--- src/app/_constants/skillset-constants.ts
+++ src/app/_constants/skillset-constants.ts
@@ -1,111 +1,129 @@
 export const SKILLSET_CARDS = [
   {
     title: 'JS',
-    proficiency: '70'
+    proficiency: '65',
+    exp: 'more than 5 projects'
   },
   {
     title: 'Angular',
-    proficiency: '25'
+    proficiency: '20',
+    exp: '2 projects'
   },
   {
     title: 'HTML 5',
-    proficiency: '50'
+    proficiency: '55',
+    exp: 'more than 5 projects'
   },
   {
     title: 'CSS 3',
-    proficiency: '70'
+    proficiency: '75',
+    exp: 'more than 5 projects'
   },
-  
+  {
+    title: 'RWD',
+    proficiency: '80',
+    exp: 'more than 5 projects'
+  },
   {
     title: 'Node JS',
-    proficiency: '10'
+    proficiency: '15',
+    exp: '1 project'
   },
   {
     title: 'Vue JS',
-    proficiency: '10'
+    proficiency: '15',
+    exp: 'N/A'
   },
   {
     title: 'Angular JS',
-    proficiency: '40'
+    proficiency: '50',
+    exp: '1 project'
   },
   {
-    title: 'React JS',
-    proficiency: '5'
+    title: 'React JS - JSX',
+    proficiency: '5',
+    exp: 'N/A'
   },
   {
     title: 'TypeScript',
-    proficiency: '20'
+    proficiency: '35',
+    exp: '2 projects'
   },
   {
     title: 'TDD - Jasmine',
-    proficiency: '5'
+    proficiency: '5',
+    exp: 'N/A'
   },
   {
     title: 'SQL',
-    proficiency: '30'
+    proficiency: '50',
+    exp: '3 projects'
   },
   {
     title: 'Mongo DB',
-    proficiency: '15'
+    proficiency: '15',
+    exp: '1 project'
   },
   {
     title: 'SCSS',
-    proficiency: '20'
+    proficiency: '50',
+    exp: '2 projects'
   },
+  // {
+  //   title: 'UX',
+  //   proficiency: '5'
+  // },
   {
-    title: 'UX',
-    proficiency: '5'
+    title: 'Express, EJS',
+    proficiency: '15',
+    exp: '1 project'
   },
-  {
-    title: 'EJS',
-    proficiency: '5'
-  },
-  {
-    title: 'Scala',
-    proficiency: '5'
-  },
-  {
-    title: 'Dev Ops',
-    proficiency: '5'
-  },
-  {
-    title: 'Project Management',
-    proficiency: '10'
-  },
-  {
-    title: 'Postgres',
-    proficiency: '5'
-  },
-  {
-    title: 'Shell',
-    proficiency: '5'
-  },
-  {
-    title: 'Regex',
-    proficiency: '25'
-  },
-  {
-    title: 'GraphQL',
-    proficiency: '5'
-  },
-  {
-    title: 'ASP.NET',
-    proficiency: '20'
-  },
-  {
-    title: 'C#',
-    proficiency: '20'
-  },
-  {
-    title: 'Python',
-    proficiency: '5'
-  },
-  {
-    title: 'Java',
-    proficiency: '5'
-  },
-  {
-    title: 'PHP',
-    proficiency: '5'
-  },
+  // {
+  //   title: 'Scala',
+  //   proficiency: '5'
+  // },
+  // {
+  //   title: 'Dev Ops',
+  //   proficiency: '5'
+  // },
+  // {
+  //   title: 'Project Management',
+  //   proficiency: '10'
+  // },
+  // {
+  //   title: 'Postgres',
+  //   proficiency: '5'
+  // },
+  // {
+  //   title: 'Shell',
+  //   proficiency: '5'
+  // },
+  // {
+  //   title: 'Regex',
+  //   proficiency: '25'
+  // },
+  // {
+  //   title: 'GraphQL',
+  //   proficiency: '5'
+  // },
+  // {
+  //   title: 'ASP.NET',
+  //   proficiency: '20'
+  // },
+  // {
+  //   title: 'C#',
+  //   proficiency: '20'
+  // },
+  // {
+  //   title: 'Python',
+  //   proficiency: '5'
+  // },
+  // {
+  //   title: 'Java',
+  //   proficiency: '5'
+  // },
+  // {
+  //   title: 'PHP',
+  //   proficiency: '5'
+  // },
 ]
