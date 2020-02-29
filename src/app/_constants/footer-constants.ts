54920cfcf958977c085a1e425fb8453755327e01
ppm1988 <ppm.career@gmail.com>
2020-02-29 18:19:37 +0530

    footer constants WIP

---
+++ src/app/_constants/footer-constants.ts
@@ -0,0 +1,23 @@
+export const FOOTER_CONSTANTS = {
+  QUOTES: [
+    {
+      link: "/past",
+      name: "# past",
+      icon: 'past'
+    },
+    {
+      link: "/skill-set",
+      name: "# skills",
+      icon: 'skillset'
+    },
+    // {
+    //   link: "/about",
+    //   name: "About me"
+    // },
+    {
+      link: "/about",
+      name: "# about",
+      icon: 'about'
+    }
+  ]
+};
