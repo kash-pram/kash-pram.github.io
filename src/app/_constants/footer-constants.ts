dd024437c2dd499819b9948a262381e72b5d7c5f
ppm1988 <ppm.career@gmail.com>
2020-03-11 11:10:17 +0530

    added couplet

--- src/app/_constants/footer-constants.ts
+++ src/app/_constants/footer-constants.ts
@@ -1,23 +1,48 @@
 export const FOOTER_CONSTANTS = {
   QUOTES: [
     {
-      link: "/past",
-      name: "# past",
-      icon: 'past'
+      tamil: "தெய்வத்தான் ஆகா தெனினும் முயற்சிதன் மெய்வருத்தக் கூலி தரும்",
+      eng: "Although it be said that, through fate, it cannot be attained, yet labour, with bodily exertion, will yield its reward",
     },
     {
-      link: "/skill-set",
-      name: "# skills",
-      icon: 'skillset'
+      tamil: "எந்நன்றி கொன்றார்க்கும் உய்வுண்டாம் உய்வில்லை செய்ந்நன்றி கொன்ற மகற்கு",
+      eng: "He who has killed every virtue may yet escape; there is no escape for him who has killed a benefit",
     },
-    // {
-    //   link: "/about",
-    //   name: "About me"
-    // },
     {
-      link: "/about",
-      name: "# about",
-      icon: 'about'
-    }
+      tamil: "நன்றி மறப்பது நன்றன்று நன்றல்லது அன்றே மறப்பது நன்று",
+      eng: "It is not good to forget a benefit; it is good to forget an injury even in the very moment",
+    },
+    {
+      tamil: 'ஒழுக்கம் விழுப்பந் தரலான் ஒழுக்கம் உயிரினும் ஓம்பப் படும்',
+      eng: 'Propriety of conduct leads to eminence, it should therefore be preserved more carefully than life'
+    },
+    {
+      tamil: 'தீயினாற் சுட்டபுண் உள்ளாறும் ஆறாதே நாவினாற் சுட்ட வடு',
+      eng: 'The wound which has been burnt in by fire may heal, but a wound burnt in by the tongue will never heal'
+    },
+    {
+      tamil: 'யாகாவா ராயினும் நாகாக்க காவாக்காற் சோகாப்பர் சொல்லிழுக்குப் பட்டு',
+      eng: 'Whatever besides you leave unguarded, guard your tongue; otherwise errors of speech and the consequent misery will ensue'
+    },
+    {
+      tamil: 'அடக்கம் அமரருள் உய்க்கும் அடங்காமை ஆரிருள் உய்த்து விடும்',
+      eng: 'Self-control will place (a man) among the Gods; the want of it will drive (him) into the thickest darkness'
+    },
+    {
+      tamil: 'அறிவினான் ஆகுவ துண்டோ பிறிதின்நோய் தந்நோய்போல் போற்றாக் கடை',
+      eng: 'What benefit has he derived from his knowledge, who does not endeavour to keep off pain from another as much as from himself ?'
+    },
+    {
+      tamil: ' இனிய உளவாக இன்னாத கூறல் கனிஇருப்பக் காய்கவர்ந் தற்று',
+      eng: 'To say disagreeable things when agreeable are at hand is like eating unripe fruit when there is ripe'
+    },
+    {
+      tamil: 'நிறையுடைமை நீங்காமை வேண்டின் பொறையுடைமை போற்றி யொழுகப் படும்',
+      eng: 'If you desire that greatness should never leave, you preserve in your conduct the exercise of patience'
+    },
+    {
+      tamil: 'அகழ்வாரைத் தாங்கும் நிலம்போலத் தம்மை இகழ்வார்ப் பொறுத்தல் தலை',
+      eng: 'To bear with those who revile us, just as the earth bears up those who dig it, is the first of virtues'
+    } 
   ]
 };
