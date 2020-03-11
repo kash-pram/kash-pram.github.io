e2b2b2e7b72a67c7e0bd3d8f9922d4a29b23242f
ppm1988 <ppm.career@gmail.com>
2020-03-11 19:47:20 +0530

    added tv shows

--- src/app/_constants/about-constants.ts
+++ src/app/_constants/about-constants.ts
@@ -1,6 +1,6 @@
 export const PROFILES_CARDS = [
   {
-    title: '-- Networks',
+    title: '-- Networks --',
     innerHTML: [
       '<a class="d-inline-block p-3 no-outline color-item" href="https://stackoverflow.com/users/2959821/ppm1988" target="_blank" title="StackOverflow"><img src="https://stackoverflow.com/users/flair/2959821.png?theme=clean" width="208" height="58" alt="StackOverflow"></a>',
       '<a class="d-inline-block p-3 no-outline color-item" href="https://github.com/ppm1988/" target="_blank" title="GitHub"><img src="assets/logos/github.png" height="58" alt="Github"></a>',
@@ -8,18 +8,18 @@ export const PROFILES_CARDS = [
     ]
   },
   {
-    title: '-- Medium Posts',
+    title: '<img class="mr-2" src="assets/logos/medium.svg" height="23" alt="Medium" title="Medium Post"> Medium Posts',
     innerHTML: [
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://medium.com/@ppm1988/how-many-colors-exist-in-the-world-bb6907d6a54e"><img class="mr-2" src="assets/logos/medium.svg" height="48" alt="Medium" title="Medium Post">How many colors exist in the world?</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://medium.com/@ppm1988/loading-webpages-faster-6950a65c6d90"><img class="mr-2" src="assets/logos/medium.svg" height="48" alt="Medium" title="Medium Post">Loading web-apps faster</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://medium.com/@ppm1988/feasibility-study-stripe-subscription-with-checkout-7ac9afd1d848"><img class="mr-2" src="assets/logos/medium.svg" height="48" alt="Medium" title="Medium Post">Feasibility study — Stripes</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://medium.com/@ppm1988/quotes-to-remember-249efd544dd6"><img class="mr-2" src="assets/logos/medium.svg" height="48" alt="Medium" title="Medium Post">Quotes to remember</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://medium.com/@ppm1988/fix-disallowed-mime-type-error-angular-github-pages-7a8e9769fe75"><img class="mr-2" src="assets/logos/medium.svg" height="48" alt="Medium" title="Medium Post">Fix — Disallowed MIME type error</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://medium.com/@ppm1988/guide-github-pages-afdfe35c190a"><img class="mr-2" src="assets/logos/medium.svg" height="48" alt="Medium" title="Medium Post">Guide — GitHub pages</a></blockquote>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://medium.com/@ppm1988/how-many-colors-exist-in-the-world-bb6907d6a54e">How many colors exist in the world?</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://medium.com/@ppm1988/loading-webpages-faster-6950a65c6d90">Loading web-apps faster</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://medium.com/@ppm1988/feasibility-study-stripe-subscription-with-checkout-7ac9afd1d848">Feasibility study — Stripes</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://medium.com/@ppm1988/quotes-to-remember-249efd544dd6">Quotes to remember</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://medium.com/@ppm1988/fix-disallowed-mime-type-error-angular-github-pages-7a8e9769fe75">Fix — Disallowed MIME type error</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://medium.com/@ppm1988/guide-github-pages-afdfe35c190a">Guide — GitHub pages</a></div>',
     ]
   },
   {
-    title: '-- Shareables',
+    title: '-- Documents --',
     innerHTML: [
       '<a class="btn btn-outline-info no-outline" target="_blank" href="https://drive.google.com/open?id=1Y-j6lISynq5pgIKkJqrES1RAWnKXSXih">Open Resume</a>',
       // LUCID CHART
@@ -27,24 +27,42 @@ export const PROFILES_CARDS = [
     ]
   },
   {
-    title: '-- YouTube Favorites',
+    title: '-- Shows --',
     innerHTML: [
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCvGYGWOKEkBWl9WB2Lg21IQ">Alexander Babu</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UC4rlAVgAK0SGk-yTfe48Qpw">Bright Side</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCNk3CeLpCA0qIZsuzGl09cw">Bruce Yeany</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCrPUg54jUy1T_wII9jgdRbg">Chris Ramsay</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UC7COwbN0rNIeLLn97jXGY0g">Claire Trips</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA">Fireship</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCIEv3lZ_tNXHzL3ox-_uUGQ">Gordan Ramsay</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCOf7UPMHBjAavgD0Qw5q5ww">HackerRank</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCZo1ciR8pZvdD3Wxp9aSNhQ">Hasura</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCc46Wo9z8S3xSDhw9vdvxtg">Jason Watmore</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UC4E1gPBh0_eJ5oftWVsewZA">Jungle Survival</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCAL3JXZSzSm8AlZyD3nQdBA">Primitive Technology</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCcYzLCs3zrQIBVHYA1sK2sw">Sadhguru</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCKuHFYu3smtrl2AwwMOXOlg">Will Smith</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCvK4bOhULCpmLabd2pDMtnA">Yes Theory</a></blockquote>',
-      '<blockquote class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCq8DICunczvLuJJq414110A">Zach King</a></blockquote>'
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.imdb.com/title/tt6226232/">Young Sheldon</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.imdb.com/title/tt0898266/">The Big Bang Theory</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.imdb.com/title/tt0108778/">f.r.i.e.n.d.s</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.imdb.com/title/tt0759364/">America\'s Got Talent</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.imdb.com/title/tt1433870/">MasterChef Australia</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.imdb.com/title/tt0455275/">Prison Break</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.imdb.com/title/tt0320037/">Jimmy Kimmel Live</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.imdb.com/title/tt1442550/">Shark Tank</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.imdb.com/title/tt0096657/">Mr. Bean</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.imdb.com/title/tt1628033/">Top Gear</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.imdb.com/title/tt0903747/">Breaking Bad</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.imdb.com/title/tt0379623/">The Ellen DeGeneres Show</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.imdb.com/title/tt1475582/">Sherlock</a></div>',
+    ]
+  },
+  {
+    title: '-- YouTube --',
+    innerHTML: [
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCvGYGWOKEkBWl9WB2Lg21IQ">Alexander Babu</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UC4rlAVgAK0SGk-yTfe48Qpw">Bright Side</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCNk3CeLpCA0qIZsuzGl09cw">Bruce Yeany</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCrPUg54jUy1T_wII9jgdRbg">Chris Ramsay</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UC7COwbN0rNIeLLn97jXGY0g">Claire Trips</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA">Fireship</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCIEv3lZ_tNXHzL3ox-_uUGQ">Gordan Ramsay</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCOf7UPMHBjAavgD0Qw5q5ww">HackerRank</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCZo1ciR8pZvdD3Wxp9aSNhQ">Hasura</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCc46Wo9z8S3xSDhw9vdvxtg">Jason Watmore</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UC4E1gPBh0_eJ5oftWVsewZA">Jungle Survival</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCAL3JXZSzSm8AlZyD3nQdBA">Primitive Technology</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCcYzLCs3zrQIBVHYA1sK2sw">Sadhguru</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCKuHFYu3smtrl2AwwMOXOlg">Will Smith</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCvK4bOhULCpmLabd2pDMtnA">Yes Theory</a></div>',
+      '<div class="text-left"><a class="no-outline" target="_blank" href="https://www.youtube.com/channel/UCq8DICunczvLuJJq414110A">Zach King</a></div>'
     ]
   }
 ];
