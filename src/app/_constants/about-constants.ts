a8fd2a365092c540f4c7b0efc3c2867d670ebda1
ppm1988 <ppm.career@gmail.com>
2020-03-11 13:25:52 +0530

    added card columns

--- src/app/_constants/about-constants.ts
+++ src/app/_constants/about-constants.ts
@@ -1,73 +1,26 @@
 export const PROFILES_CARDS = [
   {
     title: 'Networks',
-    // link: 'https://stackoverflow.com/users/2959821/ppm1988',
-    innerHTML: ['<img src="https://stackoverflow.com/users/flair/2959821.png?theme=clean" width="208" height="58" alt="" title="">',
-    '<img src="assets/logos/GitHub-Mark-120px-plus.png" height="58" alt="Github" title="">',
-    '<img src="assets/logos/LI-In-Bug.png" height="58" alt="LinkedIn" title="">',
-    '<img src="assets/logos/Twitter_Social_Icon_Circle_Color.svg" height="58" alt="Twitter" title="">'
+    innerHTML: [
+      '<img src="https://stackoverflow.com/users/flair/2959821.png?theme=clean" width="208" height="58" alt="" title="">',
+      '<img src="assets/logos/GitHub-Mark-120px-plus.png" height="58" alt="Github" title="">',
+      '<img src="assets/logos/LI-In-Bug.png" height="58" alt="LinkedIn" title="">',
+      '<img src="assets/logos/Twitter_Social_Icon_Circle_Color.svg" height="58" alt="Twitter" title="">'
     ]
   },
-  // {
-  //   title: 'Github',
-  //   link: 'https://github.com/ppm1988/',
-  //   innerHTML: 
-  // },
-  // {
-  //   title: 'LinkedIn',
-  //   link: 'https://www.linkedin.com/in/ppm1988',
-  //   innerHTML: 
-  // },
-  // {
-  //   title: 'Medium',
-  //   link: 'https://medium.com/@ppm1988',
-  //   innerHTML: '<img src="assets/logos/Monogram.svg" height="58" alt="Medium" title="">'
-  // },
   {
     title: 'Medium Posts',
-    // link: 'https://medium.com/@ppm1988/quotes-to-remember-249efd544dd6',
     innerHTML: [
-      '<blockquote class=""><h4><a href="https://medium.com/@ppm1988/quotes-to-remember-249efd544dd6">Quotes to remember</a></h4></blockquote>',
-      '<blockquote class=""><h4><a href="https://medium.com/@ppm1988/feasibility-study-stripe-subscription-with-checkout-7ac9afd1d848">Feasibility study — Stripe</a></h4></blockquote>',
-      '<blockquote class=""><h4><a href="https://medium.com/@ppm1988/fix-disallowed-mime-type-error-angular-github-pages-7a8e9769fe75">Fix — Disallowed MIME type error</a></h4></blockquote>',
-      '<blockquote class=""><h4><a href="https://medium.com/@ppm1988/how-many-colors-exist-in-the-world-bb6907d6a54e">How many colors exist in the world?</a></h4></blockquote>',
-      '<blockquote class=""><h4><a href="https://medium.com/@ppm1988/loading-webpages-faster-6950a65c6d90">Loading web-apps faster</a></h4></blockquote>',
-      '<blockquote class=""><h4><a href="https://medium.com/@ppm1988/guide-github-pages-afdfe35c190a">Guide - GitHub pages</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://medium.com/@ppm1988/quotes-to-remember-249efd544dd6">Quotes to remember</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://medium.com/@ppm1988/feasibility-study-stripe-subscription-with-checkout-7ac9afd1d848">Feasibility study — Stripe</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://medium.com/@ppm1988/fix-disallowed-mime-type-error-angular-github-pages-7a8e9769fe75">Fix — Disallowed MIME type error</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://medium.com/@ppm1988/how-many-colors-exist-in-the-world-bb6907d6a54e">How many colors exist in the world?</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://medium.com/@ppm1988/loading-webpages-faster-6950a65c6d90">Loading web-apps faster</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://medium.com/@ppm1988/guide-github-pages-afdfe35c190a">Guide - GitHub pages</a></h4></blockquote>',
     ]
   },
-  // {
-  //   title: 'Medium Post',
-  //   link: 'https://medium.com/@ppm1988/feasibility-study-stripe-subscription-with-checkout-7ac9afd1d848',
-  //   innerHTML: 
-  // },
-  // {
-  //   title: 'Medium Post',
-  //   link: 'https://medium.com/@ppm1988/fix-disallowed-mime-type-error-angular-github-pages-7a8e9769fe75',
-  //   innerHTML: 
-  // },
-  // {
-  //   title: 'Medium Post',
-  //   link: 'https://medium.com/@ppm1988/how-many-colors-exist-in-the-world-bb6907d6a54e',
-  //   innerHTML: 
-  // },
-  // {
-  //   title: 'Medium Post',
-  //   link: 'https://medium.com/@ppm1988/loading-webpages-faster-6950a65c6d90',
-  //   innerHTML: 
-  // },
-  // {
-  //   title: 'Medium Post',
-  //   link: 'https://medium.com/@ppm1988/guide-github-pages-afdfe35c190a',
-  //   innerHTML: 
-  // },
-  // {
-  //   title: 'Twitter',
-  //   link: 'https://twitter.com/social_ppm',
-  //   innerHTML: 
-  // },
   {
     title: 'Shareables',
-    // link: 
     innerHTML: [
       'https://drive.google.com/open?id=1Y-j6lISynq5pgIKkJqrES1RAWnKXSXih',
       // LUCID CHART
@@ -76,30 +29,26 @@ export const PROFILES_CARDS = [
   },
   {
     title: 'Favorites',
-    // link: 
-    innerHTML: {
-'Jason Watmore': 'https://www.youtube.com/channel/UCc46Wo9z8S3xSDhw9vdvxtg',
-'Claire Trips': 'https://www.youtube.com/channel/UC7COwbN0rNIeLLn97jXGY0g',
-'Hacker Rank': 'https://www.youtube.com/channel/UCOf7UPMHBjAavgD0Qw5q5ww',
-'Hasura': 'https://www.youtube.com/channel/UCZo1ciR8pZvdD3Wxp9aSNhQ',
-'Alexander Babu': 'https://www.youtube.com/channel/UCvGYGWOKEkBWl9WB2Lg21IQ',
-'Bright Side': 'https://www.youtube.com/channel/UC4rlAVgAK0SGk-yTfe48Qpw',
-'Bruce Yeany': 'https://www.youtube.com/channel/UCNk3CeLpCA0qIZsuzGl09cw',
-'Chris Ramsay': 'https://www.youtube.com/channel/UCrPUg54jUy1T_wII9jgdRbg',
-'Fireship': 'https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA',
-'Gordan Ramsay': 'https://www.youtube.com/channel/UCIEv3lZ_tNXHzL3ox-_uUGQ',
-'Jungle Survival': 'https://www.youtube.com/channel/UC4E1gPBh0_eJ5oftWVsewZA',
-'Primitive Technology': 'https://www.youtube.com/channel/UCAL3JXZSzSm8AlZyD3nQdBA',
-'Sadhguru': 'https://www.youtube.com/channel/UCcYzLCs3zrQIBVHYA1sK2sw',
-'Yes Theory': 'https://www.youtube.com/channel/UCvK4bOhULCpmLabd2pDMtnA',
-'Zach King': 'https://www.youtube.com/channel/UCq8DICunczvLuJJq414110A',
-'Will Smith': 'https://www.youtube.com/channel/UCKuHFYu3smtrl2AwwMOXOlg'
-    }
-      ,
-    
+    innerHTML: [
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UCc46Wo9z8S3xSDhw9vdvxtg">Jason Watmore</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UC7COwbN0rNIeLLn97jXGY0g">Claire Trips</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UCOf7UPMHBjAavgD0Qw5q5ww">Hacker Rank</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UCZo1ciR8pZvdD3Wxp9aSNhQ">Hasura</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UCvGYGWOKEkBWl9WB2Lg21IQ">Alexander Babu</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UC4rlAVgAK0SGk-yTfe48Qpw">Bright Side</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UCNk3CeLpCA0qIZsuzGl09cw">Bruce Yeany</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UCrPUg54jUy1T_wII9jgdRbg">Chris Ramsay</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA">Fireship</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UCIEv3lZ_tNXHzL3ox-_uUGQ">Gordan Ramsay</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UC4E1gPBh0_eJ5oftWVsewZA">Jungle Survival</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UCAL3JXZSzSm8AlZyD3nQdBA">Primitive Technology</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UCcYzLCs3zrQIBVHYA1sK2sw">Sadhguru</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UCvK4bOhULCpmLabd2pDMtnA">Yes Theory</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UCq8DICunczvLuJJq414110A">Zach King</a></h4></blockquote>',
+      '<blockquote class=""><h4><a target="_blank" href="https://www.youtube.com/channel/UCKuHFYu3smtrl2AwwMOXOlg">Will Smith</a></h4></blockquote>'
+    ]
   }
 ];
-
 
 export const INTERESTS_CARDS = [
   {
