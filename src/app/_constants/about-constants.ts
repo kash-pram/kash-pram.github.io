5dc19924735cc0cdf8b7d7fcdb586392dd8a2233
ppm1988 <ppm.career@gmail.com>
2020-03-12 09:52:40 +0530

    commented out favorites

--- src/app/_constants/about-constants.ts
+++ src/app/_constants/about-constants.ts
@@ -1,11 +1,11 @@
 export const PROFILES_CARDS = [
   {
-    title: '-- Networks --',
+    title: '-- Profiles --',
     innerHTML: [
-      '<a class="d-block no-outline hover-raise" href="https://www.linkedin.com/in/ppm1988" target="_blank" title="LinkedIn"><img src="assets/logos/linkedin.png" height="58" alt="LinkedIn"></a>',
+      '<a class="d-block no-outline hover-raise" href="https://www.linkedin.com/in/ppm1988" target="_blank" title="LinkedIn"><img src="assets/logos/linkedin.png" height="46" alt="LinkedIn"></a>',
       '<a class="d-block no-outline hover-raise" href="https://stackoverflow.com/users/2959821/ppm1988" target="_blank" title="StackOverflow"><img src="https://stackoverflow.com/users/flair/2959821.png?theme=clean" width="208" height="58" alt="StackOverflow"></a>',
       
-      '<a class="d-block no-outline hover-raise" href="https://github.com/ppm1988/" target="_blank" title="GitHub"><img src="assets/logos/github.png" height="58" alt="Github"></a>',
+      '<a class="d-block no-outline hover-raise" href="https://github.com/ppm1988/" target="_blank" title="GitHub"><img src="assets/logos/github.png" height="46" alt="Github"></a>',
       
     ]
   },
@@ -29,45 +29,45 @@ export const PROFILES_CARDS = [
       // FLOW DIAGRAMS
     ]
   },
-  {
-    title: '-- Shows --',
-    innerHTML: [
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0759364/">America\'s Got Talent</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0903747/">Breaking Bad</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0108778/">F.r.i.e.n.d.s</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0320037/">Jimmy Kimmel Live</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt1433870/">MasterChef Australia</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0096657/">Mr. Bean</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0455275/">Prison Break</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt1442550/">Shark Tank</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt1475582/">Sherlock</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0898266/">The Big Bang Theory</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0379623/">The Ellen DeGeneres Show</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt1628033/">Top Gear</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt6226232/">Young Sheldon</a></div>',
-    ]
-  },
-  {
-    title: '-- YouTube --',
-    innerHTML: [
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCvGYGWOKEkBWl9WB2Lg21IQ">Alexander Babu</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UC4rlAVgAK0SGk-yTfe48Qpw">Bright Side</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCNk3CeLpCA0qIZsuzGl09cw">Bruce Yeany</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCrPUg54jUy1T_wII9jgdRbg">Chris Ramsay</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UC7COwbN0rNIeLLn97jXGY0g">Claire Trips</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA">Fireship</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCIEv3lZ_tNXHzL3ox-_uUGQ">Gordan Ramsay</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCOf7UPMHBjAavgD0Qw5q5ww">HackerRank</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCZo1ciR8pZvdD3Wxp9aSNhQ">Hasura</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCc46Wo9z8S3xSDhw9vdvxtg">Jason Watmore</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UC4E1gPBh0_eJ5oftWVsewZA">Jungle Survival</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCAL3JXZSzSm8AlZyD3nQdBA">Primitive Technology</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCcYzLCs3zrQIBVHYA1sK2sw">Sadhguru</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCKuHFYu3smtrl2AwwMOXOlg">Will Smith</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCvK4bOhULCpmLabd2pDMtnA">Yes Theory</a></div>',
-      '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCq8DICunczvLuJJq414110A">Zach King</a></div>'
-    ]
-  }
+  // {
+  //   title: '-- Shows --',
+  //   innerHTML: [
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0759364/">America\'s Got Talent</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0903747/">Breaking Bad</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0108778/">F.r.i.e.n.d.s</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0320037/">Jimmy Kimmel Live</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt1433870/">MasterChef Australia</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0096657/">Mr. Bean</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0455275/">Prison Break</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt1442550/">Shark Tank</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt1475582/">Sherlock</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0898266/">The Big Bang Theory</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt0379623/">The Ellen DeGeneres Show</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt1628033/">Top Gear</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.imdb.com/title/tt6226232/">Young Sheldon</a></div>',
+  //   ]
+  // },
+  // {
+  //   title: '-- YouTube --',
+  //   innerHTML: [
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCvGYGWOKEkBWl9WB2Lg21IQ">Alexander Babu</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UC4rlAVgAK0SGk-yTfe48Qpw">Bright Side</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCNk3CeLpCA0qIZsuzGl09cw">Bruce Yeany</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCrPUg54jUy1T_wII9jgdRbg">Chris Ramsay</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UC7COwbN0rNIeLLn97jXGY0g">Claire Trips</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA">Fireship</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCIEv3lZ_tNXHzL3ox-_uUGQ">Gordan Ramsay</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCOf7UPMHBjAavgD0Qw5q5ww">HackerRank</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCZo1ciR8pZvdD3Wxp9aSNhQ">Hasura</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCc46Wo9z8S3xSDhw9vdvxtg">Jason Watmore</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UC4E1gPBh0_eJ5oftWVsewZA">Jungle Survival</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCAL3JXZSzSm8AlZyD3nQdBA">Primitive Technology</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCcYzLCs3zrQIBVHYA1sK2sw">Sadhguru</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCKuHFYu3smtrl2AwwMOXOlg">Will Smith</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCvK4bOhULCpmLabd2pDMtnA">Yes Theory</a></div>',
+  //     '<div class="text-left"><a class="no-outline text-muted" target="_blank" href="https://www.youtube.com/channel/UCq8DICunczvLuJJq414110A">Zach King</a></div>'
+  //   ]
+  // }
 ];
 
 export const INTERESTS_CARDS = [
